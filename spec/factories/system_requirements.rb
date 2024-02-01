c79e39731f727550938afd0dd00e7e9d882f6157
samuelferreiras <samuelmarketingd@gmail.com>
2024-02-01 13:43:45 -0300

    configuring new model system_requirements

--- spec/factories/system_requirements.rb
+++ spec/factories/system_requirements.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :system_requirement do
-    name { "MyString" }
-    operational_system { "MyString" }
-    storage { "MyString" }
-    processor { "MyString" }
-    memory { "MyString" }
-    video_board { "MyString" }
+    sequence(:name) { |n| "Basic #{n}" }
+    operational_system { Faker::Computer.os }
+    storage { "5GB" }
+    processor { "AMD Ryzen 7" }
+    memory { "2GB" }
+    video_board { "N/A" }
   end
 end
